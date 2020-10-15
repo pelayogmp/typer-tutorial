7253b221dc9028d35716600e4ba21c7e90ece0b7
Pelayo Gonzalez <pelayo.gonzalez@bezzier.com>
2020-10-15 21:08:48 +0200

    Tutorial first steps : Add documentation

--- src/main.py
+++ src/main.py
@@ -2,6 +2,11 @@ import typer
 
 
 def main(name: str, lastname: str = "", formal: bool = False):
+    """
+    Say hi to NAME, optionally with a --lastname.
+
+    If --formal is used, say hi very formally.
+    """
     if formal:
         typer.echo(f"Good day Ms. {name} {lastname}")
     else:
