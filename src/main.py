66b0d0469e00867b3d779f913a200ec3c724c947
Pelayo Gonzalez <pelayo.gonzalez@bezzier.com>
2020-10-15 21:05:02 +0200

    Tutorial first steps : Add an option

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 import typer
 
 
-def main(name: str, lastname: str):
-    typer.echo(f"Hello {name} {lastname}")
-
+def main(name: str, lastname: str, formal: bool = False):
+    if formal:
+        typer.echo(f"Good day Ms. {name} {lastname}")
+    else:
+        typer.echo(f"Hello {name} {lastname}")
 
 if __name__ == "__main__":
     typer.run(main)
