8d2ed090c3da197e1c279fbe9617aa1016ad638a
Pelayo Gonzalez <pelayo.gonzalez@bezzier.com>
2020-10-15 21:40:36 +0200

    Tutorial Terminating : Abort a CLI program

--- src/main.py
+++ src/main.py
@@ -14,8 +14,13 @@ def maybe_create_user(username: str):
 def send_new_user_notification(username: str):
     typer.echo(f"Notification sent for new user: {username}")
 
+def check_root(username: str):
+    if username == "root":
+        typer.secho("The root user is reserved!", err=True, fg=typer.colors.RED)
+        raise typer.Abort()
 
 def main(username: str):
+    check_root(username=username)
     maybe_create_user(username=username)
     send_new_user_notification(username=username)
     
