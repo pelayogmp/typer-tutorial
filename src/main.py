407eb2e02c0c7de2b1206e1a8579ab7f4170ffb8
Pelayo Gonzalez <pelayo.gonzalez@bezzier.com>
2020-10-15 21:01:57 +0200

    Tutorial first steps : Two CLI arguments

--- src/main.py
+++ src/main.py
@@ -1,19 +1,9 @@
 import typer
 
-app = typer.Typer()
 
-
-@app.command()
-def hello(name: str):
-    typer.echo(f"Hello {name}")
-    
-@app.command()
-def goodbye(name: str, formal: bool=False):
-    if formal:
-        typer.echo(f"Goobye Mr(s). {name}. Have a good day.")
-    else:
-        typer.echo(f"Bye {name}!")
+def main(name: str, lastname: str):
+    typer.echo(f"Hello {name} {lastname}")
 
 
 if __name__ == "__main__":
-    app()
+    typer.run(main)
