e73d33225ecb3bf73dac39e5c45dfaf59be8bbb0
Pelayo Gonzalez <pelayo.gonzalez@bezzier.com>
2020-10-15 21:36:47 +0200

    Tutorial Terminating : Exit a CLI program

--- src/main.py
+++ src/main.py
@@ -1,16 +1,23 @@
 import typer
 
+existing_usernames = ["rick", "morty"]
 
-def main(good: bool = True):
-    message_start = "everithing is "
-    if good:
-        ending = typer.style("good", fg=typer.colors.GREEN, bold=True)
+
+def maybe_create_user(username: str):
+    if username in existing_usernames:
+        typer.echo("The user already exists")
+        raise typer.Exit(code=1)
     else:
-        ending = typer.style("bad", fg=typer.colors.WHITE, bg=typer.colors.RED)
-    
-    message = message_start + ending
-    typer.echo(message)
-    typer.secho(f"This is printed to stderr ...", err=True, fg=typer.colors.MAGENTA, bg=typer.colors.WHITE)
+        typer.echo(f"User created: {username}")
 
+
+def send_new_user_notification(username: str):
+    typer.echo(f"Notification sent for new user: {username}")
+
+
+def main(username: str):
+    maybe_create_user(username=username)
+    send_new_user_notification(username=username)
+    
 if __name__ == "__main__":
     typer.run(main)
