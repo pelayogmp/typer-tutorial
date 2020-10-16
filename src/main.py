ec0a3f1c1b42f652438b04e0c5a40f46e150a418
Pelayo Gonzalez <pelayo.gonzalez@bezzier.com>
2020-10-16 13:23:15 +0200

    Tutorial CLI Options - Password promt confirmation and hide_input

--- src/main.py
+++ src/main.py
@@ -9,15 +9,15 @@ existing_usernames = ["rick", "morty"]
 def get_random_role() -> str:
     return random.choice(["HERO", "KING", "CAPTAIN", "SOLDIER", "TRAITOR"])
 
-def maybe_create_user(username: str, password: str, country: str, cfg_dir: str, nickname: Optional[str], role: str=get_random_role):
+def maybe_create_user(username: str, password: str, email: str, country: str, cfg_dir: str, nickname: Optional[str], role: str=get_random_role):
     if username in existing_usernames:
         typer.echo("The user already exists")
         raise typer.Exit(code=1)
     else:
         if nickname:
-            typer.echo(f"User created: {username} with nick '{nickname}', password {password}, role {role}, operates in {country} with cfg dir at {cfg_dir}")
+            typer.echo(f"User created: {username} with nick '{nickname}', password {password}, email {email}, role {role}, operates in {country} with cfg dir at {cfg_dir}")
         else:
-            typer.echo(f"User created: {username} without nick, password {password}, role {role} and operates in {country} with cfg dir at {cfg_dir}")
+            typer.echo(f"User created: {username} without nick, password {password}, email {email}, role {role} and operates in {country} with cfg dir at {cfg_dir}")
 
 
 def send_new_user_notification(username: str):
@@ -45,7 +45,9 @@ def main(
     # promp may be boolean or string
     country: str = typer.Option(..., help="Area of operation", prompt="Enter your area of operation"),
     # You can prompt for confimation too
-    password: str = typer.Option(..., help="Your password", prompt="Enter your password", confirmation_prompt=True),
+    email: str = typer.Option(..., help="Your email", prompt="Enter your email", confirmation_prompt=True),
+    # You can prompt and hide input too
+    password: str = typer.Option(..., help="Your password", prompt="Enter your password", confirmation_prompt=True, hide_input=True),
     # You can hide an argument in help...
     hidden_in_help: str = typer.Argument("NotShownInHelp", hidden=True),
     # You can retrieve arguments from a list of env vars, use first found. and hide them from help
@@ -55,7 +57,7 @@ def main(
     (Fake) Register new user for War of Empires MMCCCIII
     """
     check_root(username=username)
-    maybe_create_user(username=username, password=password, nickname=nickname, role=role, country=country, cfg_dir=cfg_dir)
+    maybe_create_user(username=username, password=password, nickname=nickname, email=email, role=role, country=country, cfg_dir=cfg_dir)
     send_new_user_notification(username=username)
     
 if __name__ == "__main__":
