b8d83f0ec5734d0ef87a0c3db8548173e17262fe
Pelayo Gonzalez <pelayo.gonzalez@bezzier.com>
2020-10-16 12:26:04 +0200

    Tutorial CLI Arguments - With Help metavar and hidden

--- src/main.py
+++ src/main.py
@@ -9,15 +9,15 @@ existing_usernames = ["rick", "morty"]
 def get_random_role() -> str:
     return random.choice(["HERO", "KING", "CAPTAIN", "SOLDIER", "TRAITOR"])
 
-def maybe_create_user(username: str, nickname: Optional[str], role: str=get_random_role):
+def maybe_create_user(username: str, country: str, nickname: Optional[str], role: str=get_random_role):
     if username in existing_usernames:
         typer.echo("The user already exists")
         raise typer.Exit(code=1)
     else:
         if nickname:
-            typer.echo(f"User created: {username} with nick '{nickname}' and role {role}")
+            typer.echo(f"User created: {username} with nick '{nickname}', role {role} and operates in {country}")
         else:
-            typer.echo(f"User created: {username} without nick")
+            typer.echo(f"User created: {username} without nick, role {role} and operates in {country}")
 
 
 def send_new_user_notification(username: str):
@@ -31,15 +31,25 @@ def check_root(username: str):
 # typer.Argument should be used to define argument help and other properties. 
 def main(
     # the first arg to typer.Argument is the default value, if '...' then username is required
-    username: str = typer.Argument(..., help="The required user name"),
+    # Note the metavar keyword to change the CLI argument name in help
+    username: str = typer.Argument(..., help="The required user name", metavar="✨username✨"),
     # nickname is optional and defaults to None. Typehint Optional[str] is not required by typer, 
     # but may be used by editor to warn if a None is used as str
     nickname: Optional[str] = typer.Argument(None, help="The optional nickname"),
     # Dynamic default value will call get_random_role to obtain the default.
-    role: Optional[str] = typer.Argument(get_random_role, help='The role, one of ["HERO", "KING", "CAPTAIN", "SOLDIER", "TRAITOR"]')
+    role: Optional[str] = typer.Argument(get_random_role, 
+                                        help='The role, one of ["HERO", "KING", "CAPTAIN", "SOLDIER", "TRAITOR"]', 
+                                        show_default="randomly selected"),
+    # argument with default value, help and default value shown. Note that show_default may be boolean or string
+    country: str = typer.Argument("Entire World", help="Area of operation", show_default=True),
+    # You can hide an argument in help...
+    hidden_in_help: str = typer.Argument("NotShownInHelp", hidden=True)
 ):
+    """
+    (Fake) Register new user for War of Empires MMCCCIII
+    """
     check_root(username=username)
-    maybe_create_user(username=username, nickname=nickname, role=role)
+    maybe_create_user(username=username, nickname=nickname, role=role, country=country)
     send_new_user_notification(username=username)
     
 if __name__ == "__main__":
