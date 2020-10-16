8bfb2775d3a83f640864d6323f9665fb6ce19eea
Pelayo Gonzalez <pelayo.gonzalez@bezzier.com>
2020-10-16 10:59:15 +0200

    Tutorial CLI Arguments - Optional CLI Arguments

--- src/main.py
+++ src/main.py
@@ -1,14 +1,18 @@
+from typing import Optional
 import typer
 
 existing_usernames = ["rick", "morty"]
 
 
-def maybe_create_user(username: str):
+def maybe_create_user(username: str, nickname: str):
     if username in existing_usernames:
         typer.echo("The user already exists")
         raise typer.Exit(code=1)
     else:
-        typer.echo(f"User created: {username}")
+        if nickname:
+            typer.echo(f"User created: {username} with nick '{nickname}'")
+        else:
+            typer.echo(f"User created: {username} without nick")
 
 
 def send_new_user_notification(username: str):
@@ -19,9 +23,14 @@ def check_root(username: str):
         typer.secho("The root user is reserved!", err=True, fg=typer.colors.RED)
         raise typer.Abort()
 
-def main(username: str):
+# typer.Argument should be used to define argument help and other properties. 
+# the first arg to typer.Argument is the default value, if '...' then argument is required
+def main(
+    username: str = typer.Argument(..., help="The required user name"),
+    nickname: Optional[str] = typer.Argument(None, help="The optional nickname")
+):
     check_root(username=username)
-    maybe_create_user(username=username)
+    maybe_create_user(username=username, nickname=nickname)
     send_new_user_notification(username=username)
     
 if __name__ == "__main__":
